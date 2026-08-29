a9ea0aefb10e367445ce332257cb92f5cc8aca7a
soumarjyotibhuyan-arch <soumarjyotibhuyan@gmail.com>
2026-08-29 14:31:26 +0530

    style: remove default Docs link and update header brand title to My Schedule App

--- src/components/app-tabs.web.tsx
+++ src/components/app-tabs.web.tsx
@@ -55,21 +55,10 @@ export function CustomTabList(props: TabListProps) {
     <View {...props} style={styles.tabListContainer}>
       <ThemedView type="backgroundElement" style={styles.innerContainer}>
         <ThemedText type="smallBold" style={styles.brandText}>
-          Expo Starter
+          📅 My Schedule App
         </ThemedText>
 
         {props.children}
-
-        <ExternalLink href="https://docs.expo.dev" asChild>
-          <Pressable style={styles.externalPressable}>
-            <ThemedText type="link">Docs</ThemedText>
-            <SymbolView
-              tintColor={colors.text}
-              name={{ ios: 'arrow.up.right.square', web: 'link' }}
-              size={12}
-            />
-          </Pressable>
-        </ExternalLink>
       </ThemedView>
     </View>
   );
